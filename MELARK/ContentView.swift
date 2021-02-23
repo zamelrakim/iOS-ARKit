7a59cd779f6910fc97ca4ea2549afc1e99582997
Zamel Rakim <zrhill@gmail.com>
2021-02-23 00:00:28 -0500

    Experience Box - Commented out the Box from the Experience Reality File

--- MELARK/ContentView.swift
+++ MELARK/ContentView.swift
@@ -21,10 +21,10 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
